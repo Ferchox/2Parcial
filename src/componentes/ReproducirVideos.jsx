529de0d51be410abc9f8a2875f8d3f26c309ced5
Ferchox <fernandotriguero123@gmail.com>
2024-05-10 09:23:12 -0400

    primer componente

---
+++ src/componentes/ReproducirVideos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Reproducir } from "./Reproducir";
+import { Descripcion } from "./Descripcion";
+
+export const ReproducirVideos = () => {
+  return (
+    <>
+      <Reproducir />
+      <Descripcion />
+    </>
+  );
+};
