019dedd6d637d932087bb76e0e1f24d5168c5ca3
Ferchox <fernandotriguero123@gmail.com>
2024-05-10 09:25:34 -0400

    primer componente

---
+++ src/componentes/Descripcion.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Descripcion = () => {
+  return (
+    <>
+    <h1>Terapia 2</h1>
+    <p>Ejercicios pelvicos</p>
+    </>
+  )
+}
---
+++ src/componentes/Reproducir.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Reproducir = () => {
+  return (
+    <>
+      <div className="boton"></div>
+      <img></img>
+    </>
+  );
+};
